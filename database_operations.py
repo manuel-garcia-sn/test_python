cc08c2432a15831c41dd215fcfabb87738514c30
manuel.garcia <manuel.garcia@sngular.com>
2020-01-08 14:50:09 +0100

    Added CRUD tests.

--- database_operations.py
+++ database_operations.py
@@ -2,7 +2,7 @@ import sqlite3
 import os.path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "mydatabase")
+DB_PATH = os.path.join(BASE_DIR, 'mydatabase')
 NOT_STARTED = 'Not Started'
 IN_PROGRESS = 'In Progress'
 COMPLETED = 'Completed'
@@ -21,6 +21,60 @@ def add_to_list(item):
             # We commit to save the change
             conn.commit()
             return {"item": item, "status": NOT_STARTED}
+        except Exception as e:
+            print('Error: ', e)
+            return None
+
+
+def search_in_list(item_name):
+    with sqlite3.connect(DB_PATH) as conn:
+        try:
+            c = conn.cursor()
+
+            c.execute("select * from items where item='%s'" % item_name)
+            db_item = c.fetchone()
+
+            if db_item is None:
+                return None
+
+            return {"item": db_item[0], "status": db_item[1]}
+        except Exception as e:
+            print('Error: ', e)
+            return None
+
+
+def delete_item_list(item_name):
+    with sqlite3.connect(DB_PATH) as conn:
+        try:
+            c = conn.cursor()
+
+            result = c.execute("delete from items where item='%s'" % item_name)
+
+            conn.commit()
+
+            return result
+        except Exception as e:
+            print('Error: ', e)
+            return None
+
+
+def get_all_items():
+    with sqlite3.connect(DB_PATH) as conn:
+        try:
+            c = conn.cursor()
+            c.execute('select * from items')
+            rows = c.fetchall()
+            return {"count": len(rows), "items": rows}
+        except Exception as e:
+            print('Error: ', e)
+            return None
+
+
+def truncate_items():
+    with sqlite3.connect(DB_PATH) as conn:
+        try:
+            c = conn.cursor()
+            c.execute('DELETE FROM items WHERE true')
         except Exception as e:
             print('Error: ', e)
             return None
--- tests/helper_test.py
+++ tests/helper_test.py
@@ -1,13 +1,46 @@
 import unittest
+
 import database_operations
 
 
-class TestSum(unittest.TestCase):
-    def test_add_to_list(self):
-        item = {"item": 'Test Item 2', "status": database_operations.NOT_STARTED}
-        res_data = database_operations.add_to_list(item["item"])
+class TestDatabaseOperations(unittest.TestCase):
+    item = {"item": 'Test Item 1000', "status": database_operations.NOT_STARTED}
+    base_item_name = 'Test Item '
 
-        self.assertEqual(res_data, item)
+    @classmethod
+    def setUpClass(cls) -> None:
+        database_operations.truncate_items()
+
+        for x in range(100):
+            database_operations.add_to_list(cls.base_item_name + str(x))
+
+    # @classmethod
+    # def tearDownClass(cls) -> None:
+    #     database_operations.truncate_items()
+
+    def test_add_to_list(self):
+        res_data = database_operations.add_to_list(self.item["item"])
+
+        self.assertEqual(res_data, self.item)
+
+    def test_search_in_list(self):
+        item_to_search = self.base_item_name + str(10)
+        res_data = database_operations.search_in_list(item_to_search)
+
+        self.assertEqual(res_data["item"], item_to_search)
+
+    def test_delete_item(self):
+        item_to_delete = self.base_item_name + str(50)
+
+        database_operations.delete_item_list(item_to_delete)
+        res_data = database_operations.search_in_list(item_to_delete)
+
+        self.assertEqual(res_data, None)
+
+    def test_get_all_items(self):
+        items = database_operations.get_all_items()
+
+        self.assertEqual(items['count'], 100)
 
 
 if __name__ == '__main__':
