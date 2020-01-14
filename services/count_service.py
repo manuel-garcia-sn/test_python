556ac610bc387f3987a3f00c90ec36da813833af
Carlos Cruz San Román <carlos.cruz@sngular.com>
2020-01-14 13:24:24 +0100

    Cleanup counter service

--- services/count_service.py
+++ services/count_service.py
@@ -1,28 +1,56 @@
-from datetime import datetime
-
 from bson import ObjectId
 
-from clients.twitter import TwitterApi
 from mongo_client import MongoClient
 from config.settings import MONGODB_HOST, MONGODB_PORT, MONGODB_DATABASE
 
 
 class CountService:
-    @staticmethod
-    def calculate_count():
-        client = MongoClient(
+    def __init__(self):
+        self.client = MongoClient(
             host=MONGODB_HOST,
             port=MONGODB_PORT,
             db=MONGODB_DATABASE
         )
 
-        users = client.db.users.find({})
+    def calculate_count(self):
+        self._reset_counters()
+        self._process_feed()
 
-        for user in users:
-            tweets = client.db.feed.find({'_id': ObjectId(user.get('_id'))})
+    def _reset_counters(self):
+        self.client.db.users.update_many(
+            {},
+            {
+                '$set': {
+                    'retweet_count': 0,
+                    'favorite_count': 0,
+                    'tweets_count': 0
+                }
+            })
 
-            for tweet in tweets:
-                #TODO: calcular el total de counts fav + rt y actualizar el count en el documento del usuario
+    def _process_feed(self):
+        for user in self.client.db.users.find({}):
+            user_id = user.get('_id')
+            tweets = self.client.db.feed.find({'user': ObjectId(user_id)})
+            self._process_user_tweets(user_id=user_id, tweets=tweets)
+
+    def _process_user_tweets(self, user_id, tweets):
+        for tweet in tweets:
+            self._update_user_count(
+                user_id=user_id,
+                tweet=tweet,
+            )
+
+    def _update_user_count(self, user_id, tweet):
+        self.client.db.users.update_one(
+            {'_id': user_id},
+            {
+                '$inc': {
+                    'retweet_count': tweet.get('retweet_count'),
+                    'favorite_count': tweet.get('favorite_count'),
+                    'tweets_count': 1
+                }
+            }
+        )
 
 
 c = CountService()
