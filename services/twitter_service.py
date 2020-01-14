d91138a5667c4fd46e1f6683681f0fdaf192d7b6
Carlos Cruz San Román <carlos.cruz@sngular.com>
2020-01-14 11:10:22 +0100

    Find and insert user

--- services/twitter_service.py
+++ services/twitter_service.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+
+from bson import ObjectId
+
 from clients.twitter import TwitterApi
 from mongo_client import MongoClient
 from config.settings import MONGODB_HOST, MONGODB_PORT, MONGODB_DATABASE
@@ -17,6 +20,31 @@ class TwitterService:
         tweets = twitter_api.tweets(query)
 
         for tweet in tweets:
+            # create or update user
+            twitter_user = tweet.get('user')
+            found_user = client.db.users.find_one({
+                'twitter_id': twitter_user.get('id')
+            })
+
+            if found_user is None:
+                found_user = {
+                    'twitter_id': twitter_user.get('id'),
+                    'twitter_name': twitter_user.get('name'),
+                    'retweet_count': 0,
+                    'favorite_count': 0,
+                    'validated': None
+                }
+
+                user = client.db.users.insert(found_user)
+
+                print(user.twitter_id)
+
+
+
+
+
+
+
             find_urls = (tweet.get('entities').get('urls'))
 
             urls = {}
@@ -26,42 +54,39 @@ class TwitterService:
                     'expanded_url': find_urls[0].get('expanded_url'),
                 }
 
-            user = tweet.get('user')
 
-            tweet = client.db.feed.find_one({'type': 'tweet', 'internal_id': tweet.get('id')})
+            found_tweet = client.db.feed.find_one({'type': 'tweet', 'internal_id': tweet.get('id')})
 
-            if tweet:
-                tweet.update('retweet_count', tweet.get('retweet_count'))
-                tweet.update('favorite_count', tweet.get('favorite_count'))
-
+            if found_tweet:
+                print('encontrado')
                 update = {
                     'retweet_count': tweet.get('retweet_count'),
                     'favorite_count': tweet.get('favorite_count')
                 }
 
-                client.db.feed.update_one({'_id': tweet.get('_id')}, {"$set": update}, upsert=False)
+                client.db.feed.update_one({'_id': ObjectId(found_tweet.get('_id'))}, {"$set": update}, upsert=False)
 
-            # data = {
-            #     'type': 'tweet',
-            #     'internal_id': tweet.get('id'),
-            #     'title': tweet.get('text'),
-            #     'link': 'https://twitter.com/{}/status/{}'.format(
-            #         tweet.get('user').get('screen_name'),
-            #         tweet.get('id')
-            #     ),
-            #     'urls': urls,
-            #     'retweet_count': tweet.get('retweet_count'),
-            #     'favorite_count': tweet.get('favorite_count'),
-            #     'user': {
-            #         'name': user.get('name'),
-            #         'screen_name': user.get('screen_name'),
-            #         'profile_image_url': user.get('profile_image_url_https')
-            #     },
-            #     'created_at': datetime.strptime(tweet.get('created_at'), '%a %b %d %X %z %Y')
-            # }
-            #
-            # if tweet is None:
-            #     client.db.feed.insert_one(data)
+            data = {
+                'type': 'tweet',
+                'internal_id': tweet.get('id'),
+                'title': tweet.get('text'),
+                'link': 'https://twitter.com/{}/status/{}'.format(
+                    tweet.get('user').get('screen_name'),
+                    tweet.get('id')
+                ),
+                'urls': urls,
+                'retweet_count': tweet.get('retweet_count'),
+                'favorite_count': tweet.get('favorite_count'),
+                'user': {
+                    'name': twitter_user.get('name'),
+                    'screen_name': twitter_user.get('screen_name'),
+                    'profile_image_url': twitter_user.get('profile_image_url_https')
+                },
+                'created_at': datetime.strptime(tweet.get('created_at'), '%a %b %d %X %z %Y')
+            }
+
+            if found_tweet is None:
+                client.db.feed.insert_one(data)
 
 
 t = TwitterService()
