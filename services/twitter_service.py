29117223b6f69ccacc055ee19ac28a26fd42fe4b
Carlos Cruz San Román <carlos.cruz@sngular.com>
2020-01-14 11:50:22 +0100

    Update results

--- services/twitter_service.py
+++ services/twitter_service.py
@@ -35,18 +35,21 @@ class TwitterService:
                     'validated': None
                 }
 
-                user_id = client.db.users.insert_one(found_user)
+                user_id = client.db.users.insert_one(found_user).inserted_id
+                # print('user id si no existe el jambo', user_id)
             else:
+                # print('YA EXISTE')
                 user_id = found_user.get('_id')
+                # print('user id si existe el jambo', user_id)
 
-            find_urls = (tweet.get('entities').get('urls'))
-
-            urls = {}
-            if find_urls:
-                urls = {
-                    'short_url': find_urls[0].get('url'),
-                    'expanded_url': find_urls[0].get('expanded_url'),
-                }
+            # find_urls = (tweet.get('entities').get('urls'))
+            #
+            # urls = {}
+            # if find_urls:
+            #     urls = {
+            #         'short_url': find_urls[0].get('url'),
+            #         'expanded_url': find_urls[0].get('expanded_url'),
+            #    }
 
             found_tweet = client.db.feed.find_one({'type': 'tweet', 'internal_id': tweet.get('id')})
 
@@ -56,25 +59,37 @@ class TwitterService:
                     'favorite_count': tweet.get('favorite_count')
                 }
 
-                client.db.feed.update_one({'_id': ObjectId(found_tweet.get('_id'))}, {"$set": update}, upsert=False)
+                tweet_id = client.db.feed.update_one(
+                    {'_id': ObjectId(found_tweet.get('_id'))},
+                    {"$set": update},
+                    upsert=False
+                )
 
-            data = {
-                'type': 'tweet',
-                'internal_id': tweet.get('id'),
-                'title': tweet.get('text'),
-                'link': 'https://twitter.com/{}/status/{}'.format(
-                    tweet.get('user').get('screen_name'),
-                    tweet.get('id')
-                ),
-                'urls': urls,
-                'retweet_count': tweet.get('retweet_count'),
-                'favorite_count': tweet.get('favorite_count'),
-                'user': user_id,
-                'created_at': datetime.strptime(tweet.get('created_at'), '%a %b %d %X %z %Y')
-            }
+                print('updated:', found_tweet.get('_id'))
 
-            if found_tweet is None:
-                client.db.feed.insert_one(data)
+            else:
+                data = {
+                    'type': 'tweet',
+                    'internal_id': tweet.get('id'),
+                    'title': tweet.get('text'),
+                    'link': 'https://twitter.com/{}/status/{}'.format(
+                        tweet.get('user').get('screen_name'),
+                        tweet.get('id')
+                    ),
+                    # 'urls': urls,
+                    'retweet_count': tweet.get('retweet_count'),
+                    'favorite_count': tweet.get('favorite_count'),
+                    'user': user_id,
+                    'created_at': datetime.strptime(tweet.get('created_at'), '%a %b %d %X %z %Y')
+                }
+
+                tweet_id = client.db.feed.insert_one(data).inserted_id
+                print('inserted:', tweet_id)
+
+            client.db.users.update_one(
+                {'_id': ObjectId(user_id)},
+                {'$push': {'tweets':  tweet_id}}
+            )
 
 
 t = TwitterService()
