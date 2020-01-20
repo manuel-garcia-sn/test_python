01b9bca3dcb1f2535c2cf931fc8ddb86fe7d82f4
Carlos Paniagua Cano <carlos.paniagua@sngular.team>
2020-01-20 11:58:57 +0100

    Added Flask-Script dependency to requirements.txt and modify scripts python to flask scripts

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from routes import api_routes
-
+from flask_script import Manager
+from services.youtube_service import YoutubeService
+from services.twitter_service import TwitterService
 
 app = Flask(__name__)
 app.register_blueprint(api_routes)
+
+manager = Manager(app)
+
+
+if __name__ == '__main__':
+    manager.add_command('youtube_service', YoutubeService())
+    manager.add_command('twitter_service', TwitterService())
+    manager.run()
