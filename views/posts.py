5c5cffcca351eb2ff92bcf62b86206dac069d413
manuel.garcia <mjgr0013@gmail.com>
2020-02-26 13:29:59 +0100

    Added pagination to post fetch API

--- views/posts.py
+++ views/posts.py
@@ -11,10 +11,11 @@ class Posts:
     @staticmethod
     def list():
         post_type = request.args.get('type')
+        page = int(request.args.get('page', 0))
         query = request.args.get('q', 'sngularrocks')
         posts = Post()
 
-        return Response(json.dumps(posts.all(query, post_type), default=json_util.default), mimetype='application/json')
+        return Response(json.dumps(posts.all(query, page, post_type), default=json_util.default), mimetype='application/json')
 
 
 class AddPostView(View):
